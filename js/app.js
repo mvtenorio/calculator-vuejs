36edf3a5cbb047341209f7a1726cbc7fa785f9dc
Marcus Tenório <marcusvtenorio@gmail.com>
2015-11-27 11:17:41 -0200

    Refactor a few things

--- js/app.js
+++ js/app.js
@@ -51,66 +51,21 @@ var vm = new Vue({
 });
 
 var keyBindings = [
-  {
-    keys: ['0', 'num0'],
-    entry: '0'
-  },
-  {
-    keys: ['1', 'num1'],
-    entry: '1'
-  },
-  {
-    keys: ['2', 'num2'],
-    entry: '2'
-  },
-  {
-    keys: ['3', 'num3'],
-    entry: '3'
-  },
-  {
-    keys: ['4', 'num4'],
-    entry: '4'
-  },
-  {
-    keys: ['5', 'num5'],
-    entry: '5'
-  },
-  {
-    keys: ['6', 'num6'],
-    entry: '6'
-  },
-  {
-    keys: ['7', 'num7'],
-    entry: '7'
-  },
-  {
-    keys: ['8', 'num8'],
-    entry: '8'
-  },
-  {
-    keys: ['9', 'num9'],
-    entry: '9'
-  },
-  {
-    keys: ['numadd'],
-    entry: '+'
-  },
-  {
-    keys: ['numsubtract'],
-    entry: '-'
-  },
-  {
-    keys: ['nummultiply'],
-    entry: '×'
-  },
-  {
-    keys: ['numdivide'],
-    entry: '÷'
-  },
-  {
-    keys: ['numdecimal'],
-    entry: '.'
-  }
+  { keys: ['0', 'num0'], entry: '0' },
+  { keys: ['1', 'num1'], entry: '1' },
+  { keys: ['2', 'num2'], entry: '2' },
+  { keys: ['3', 'num3'], entry: '3' },
+  { keys: ['4', 'num4'], entry: '4' },
+  { keys: ['5', 'num5'], entry: '5' },
+  { keys: ['6', 'num6'], entry: '6' },
+  { keys: ['7', 'num7'], entry: '7' },
+  { keys: ['8', 'num8'], entry: '8' },
+  { keys: ['9', 'num9'], entry: '9' },
+  { keys: ['numadd'], entry: '+' },
+  { keys: ['numsubtract'], entry: '-' },
+  { keys: ['nummultiply'], entry: '×' },
+  { keys: ['numdivide'], entry: '÷' },
+  { keys: ['numdecimal'], entry: '.' }
 ];
 
 keyBindings.forEach(function(el) {
